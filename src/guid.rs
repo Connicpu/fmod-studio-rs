78d9c29259f4ce4c0b394fa024878d8bd0bdb2d8
Connie Hilarides <conni_h@outlook.com>
2017-09-10 12:26:42 -0700

    Guid Eq

--- src/guid.rs
+++ src/guid.rs
@@ -1,10 +1,13 @@
+use std::cmp::Ordering;
 use std::ffi::CString;
-use std::mem::{uninitialized, zeroed};
+use std::mem::{size_of_val, uninitialized, zeroed};
+use std::slice;
 
 use ffi::*;
 use error::*;
 
 #[derive(Copy, Clone)]
+#[repr(C)]
 pub struct Guid {
     pub inner: FMOD_GUID,
 }
@@ -27,5 +30,30 @@ impl Guid {
 impl Default for Guid {
     fn default() -> Guid {
         unsafe { zeroed() }
+    }
+}
+
+impl PartialEq for Guid {
+    fn eq(&self, rhs: &Guid) -> bool {
+        self.cmp(rhs) == Ordering::Equal
+    }
+}
+
+impl Eq for Guid {}
+
+impl PartialOrd for Guid {
+    fn partial_cmp(&self, rhs: &Guid) -> Option<Ordering> {
+        Some(self.cmp(rhs))
+    }
+}
+
+impl Ord for Guid {
+    fn cmp(&self, rhs: &Guid) -> Ordering {
+        unsafe {
+            let left = slice::from_raw_parts(self as *const _ as *const u8, size_of_val(self));
+            let right = slice::from_raw_parts(rhs as *const _ as *const u8, size_of_val(rhs));
+
+            left.cmp(right)
+        }
     }
 }
